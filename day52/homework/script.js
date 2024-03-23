7ce5d2407062a3594ffd822a9ce8a824b11b3d1b
Rostom-Chagunava <rostomchagunava1994@gmail.com>
2024-03-23 15:37:02 +0100

    52 commit

---
+++ day52/homework/script.js
@@ -0,0 +1,26 @@
+// var, ler, const - ქიუწორდ.  
+var name = "rostom" //ქიუწორდ პატარა ბრაძანება, თავისი დანიშნულებით, მისი დახმარებით იქმნევა ცვლადი var - variable-ცვლადი - let მსგავსია var რისა. განახლებული ვერისა let//
+
+
+// console.log(typeof name)
+
+let age = 30
+
+let lastName = "chagu"
+
+// == შედარების ოპერატორი === მკაცრი ტოლობა.
+
+// console.log(name.length)  // სიგრძის გაგება 
+
+// let name = Number(name)
+
+// let mane = String(name ) // ტიპის შეცვლა
+
+if (name.length === 6){
+    console.log("right")
+}else if(lastName == "chagu"){
+    console.log("right")
+}else{
+    console.log|("samsing wrong")
+}
+
