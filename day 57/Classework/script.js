205f204a1dc86382cac1ec0609e6139598989e33
Rostom-Chagunava <rostomchagunava1994@gmail.com>
2024-04-10 11:29:09 +0200

    57 commit

---
+++ day 57/Classework/script.js
@@ -0,0 +1,18 @@
+let arr = [88, 111, 34, 5, 71, 55, 1000, 1234, 99999, 345];
+
+
+
+let i = 0
+    
+let myArr = []
+    
+for (i;i < arr.lenght;i ++){
+      
+    if (i %2 == 0){
+    myArr.push(arr[i])
+  }
+}
+
+console.log(myArr)
+
+
