fe495147ceb4dda7b7644517ccac119b91c778ce
Rostom-Chagunava <rostomchagunava1994@gmail.com>
2024-05-03 18:52:21 +0200

    64 commit

---
+++ day64/clasework/script.js
@@ -0,0 +1,38 @@
+
+// let box = document.getElementById ("box")
+// let box1 = document.getElementById ("contaner1")
+// let Sum = document.getElementById("sum")
+
+// let result = 0
+
+// function moveBox(){
+//    let number = Math.floor(Math.random()*1000)
+//    let number1 = Math.floor(Math.random()*1000)
+
+//    box.style.left = number + "px"
+//    box.style.top = number1 + "px"
+
+//    if((number1 >= 100 && number1 <= 600) && (number >= 400 && number <= 800)){
+//       box1.style.border =  "solid 2px #28ce5f"
+//       result += 1
+//       Sum.innerHTML = result
+//    }else{box1.style.border =  "solid 2px #ff0000"}
+// }
+
+
+let obieqt = {
+   name: "otari",
+   surname: "kjik;l",
+   age: "56"
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ day64/homework/script.js
@@ -0,0 +1,23 @@
+let sum = document.getElementById("sum")
+
+let box = document.getElementById("box")
+let box1 = document.getElementById("box1")
+
+let result = 0
+
+function moveBox(){
+    let number = Math.floor(Math.random() * 1000)
+    let number1 = Math.floor(Math.random() * 1000)
+
+    box1.style.top = number + "px"
+    box1.style.right = number1 + "px" 
+
+    if ((number >= 100 && number <= 400) && (number1 >=450 && number1 <= 800 )){
+        result += 1
+        sum.innerHTML = result
+        box.style.border = "solid 2px #0dae0d"
+    }else{
+        box.style.border = "solid 2px #ff0000"
+    }
+
+}
