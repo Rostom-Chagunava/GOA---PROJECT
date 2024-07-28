a3199af326edb82cf8bc22269779f58b5d26ac9c
Rostom-Chagunava <rostomchagunava1994@gmail.com>
2024-07-28 06:39:31 +0200

    85 commit

---
+++ day85/Homework/scropt.js
@@ -0,0 +1,23 @@
+
+
+let myArr = [12,5,6,4,58,9,8]
+
+let myStr = "hallo"
+
+let myname = "name"
+
+
+
+myObj = {
+    name:["rostom","otar"],
+    surname:'chagu',
+    age:30,
+}
+
+// for (let i of myObj[myname]){
+//     console.log(i)
+// }
+
+for (let i in myObj){
+    console.log(myObj[i])
+}
