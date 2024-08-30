88e8a69867dbfbc86f12e3c8c2485ba7518b09ea
Rostom-Chagunava <rostomchagunava1994@gmail.com>
2024-08-30 16:36:00 +0200

    99 commit

---
+++ day99/script.js
@@ -0,0 +1,70 @@
+// const items = [
+//     { name: 'Apple', category: 'Fruit' },
+//     { name: 'Onion', category: 'Vegetable' },
+//     { name: 'Orange', category: 'Fruit' },
+//     { name: 'Lettuce', category: 'Vegetable'},
+//   ];
+
+
+//   const groupedItems = items.reduce((accumulator, item) => {
+//     console.log(item.category)
+//     const category = item.category;
+       
+//     if (!accumulator[category]){
+//         accumulator[category] = []
+//     }
+
+//     console.log(accumulator)
+//     accumulator[category].push(item.name);
+//     return accumulator
+//   }, {})
+
+//   console.log(groupedItems)
+
+
+// const items = [1, 2, 3, 1, 2, 3, 7, 8, 7];
+
+// // const noDuplicateItems = items.reduce((accumulator, item) => {
+// //   if (!accumulator.includes(item)){
+// //     accumulator.push(item);
+// //   }
+// //   return accumulator;
+// // }, []);
+
+// // console.log(noDuplicateItems); 
+
+
+// const items = [
+//   { name: 'Apple', price: 1 },
+//   { name: 'Orange', price: 2 },
+//   { name: 'Mango', price: 3 },
+// ];
+
+// const totalPrice = items.reduce((accumulator ,item) => {
+//   console.log(accumulator); // log the accumulator
+//   // return accumulator += item.price;
+// })
+
+
+
+
+
+
+// let myStr =562548
+
+
+
+
+// for(let i of String(myStr)){
+//   console.log(i)
+// }
+
+// const myArr = result.map((element) =>{
+//   return Number(element) ** 2
+// })
+
+// console.log(myArr)
+
+
+
+
