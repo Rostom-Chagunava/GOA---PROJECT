bc1c5948fea06c1b229b8a3cf0f50605c67ec349
Rostom-Chagunava <rostomchagunava1994@gmail.com>
2024-09-30 19:30:02 +0200

    103 commit

---
+++ day103/script.js
@@ -0,0 +1,57 @@
+// 1) Find the difference between two sets: Create a new set containing elements that are present in the first set but not in the second.
+// 2) Create a random number generator without duplicates: Use a set to prevent duplicate random numbers.
+
+
+
+
+
+
+
+// let arr1 = new Set ([1,2,3])
+// let arr2 =new Set ([6,1,3])
+// let myArr = []
+
+// arr1.forEach((item) =>{
+//     if(!arr2.has(item)){
+//         myArr.push(item)
+//     }
+
+// })
+
+
+
+// let  i = 10
+
+// let arr = new Set()
+
+// while( i>0){
+//     let randomNumber = Math.floor((Math.random() * i))
+//     arr.add(randomNumber)
+//     i --
+// }
+
+// let randomNumber = Math.floor((Math.random() * 56))
+
+// if(!arr1.has(arr2.values)){
+//     myArr.push(arr1.values)
+// }
+
+// console.log(arr)
+
+// let myMap = new Map ([
+//     ["n1", 5],
+//     ["m2", 8],
+//     ["u3", 14]
+// ]
+    
+// )
+
+// // console.log(myMap.entries())
+
+// for(let [i,l] of myMap.entries()){
+//     console.log(i,l)
+// }
+
+
+
+
